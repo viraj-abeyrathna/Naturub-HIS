0f6b0f73aa0eff323b60353a62e3464729aae162
Ramodk <42975445+Ramodk@users.noreply.github.com>
2022-01-13 10:17:51 +0530

    Mat Dialog
    Mat Dialog

--- src/app/pages/master/component/component.component.ts
+++ src/app/pages/master/component/component.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
-
+import { MatDialog } from '@angular/material/dialog';
+import { EditDialogComponent } from '../matdialog';
 import { ComponentService } from "../../../api-services/component.service";
+
 
 
 export interface Components {
@@ -15,6 +17,7 @@ export interface Components {
   MaxLength?: number;
   DataSetID?: number;
   DataSetName?: string
+
 }
 
 @Component({
@@ -25,6 +28,7 @@ export interface Components {
 export class ComponentComponent implements OnInit {
 
 
+
   dataSource: MatTableDataSource<Components> = new MatTableDataSource();
   components: Components[] = [];
   columns: string[] = ['ComponentID', 'ComponentName', 'ComponentTypeName','Actions'];
@@ -32,7 +36,7 @@ export class ComponentComponent implements OnInit {
   @ViewChild(MatSort, { static: false })sort: MatSort = new MatSort;
   @ViewChild(MatPaginator)paginator!: MatPaginator;
 
-  constructor(private service: ComponentService) {
+  constructor(private service: ComponentService,public dialog: MatDialog) {
 
     this.service.getGetComponentList(0).subscribe(
       (data: any) => {
@@ -42,6 +46,18 @@ export class ComponentComponent implements OnInit {
         return data;
       }
     ); 
+  }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '250px',
+      // data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
   }
 
   ngOnInit(): void {
---
+++ src/app/pages/master/matdialog/edit-dialog.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import {MatCommonModule} from '@angular/material/core';
+
+import { EditDialogComponent } from './edit-dialog.component';
+
+@NgModule({
+  declarations: [EditDialogComponent],
+  entryComponents: [EditDialogComponent],
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatCommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+})
+export class ExampleDialogModule {}
