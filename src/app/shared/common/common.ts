b98f00546e1dffed2dd63b842a510d1dedd73513
Viraj <virajlasithazeero@gmail.com>
2022-01-17 22:46:05 +0530

    .

--- src/app/shared/common/common.ts
+++ src/app/shared/common/common.ts
@@ -1,41 +1,43 @@
-import { NgModule} from '@angular/core'; 
+import { NgModule } from '@angular/core';
 
 // Material UI Modules
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatRippleModule} from '@angular/material/core';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatTableModule} from '@angular/material/table';
-import {MatFormFieldModule} from '@angular/material/form-field'; 
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatSortModule} from '@angular/material/sort';
-import {MatInputModule} from '@angular/material/input';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-    exports:[
-      MatToolbarModule,
-      MatIconModule,
-      MatButtonModule,
-      MatSidenavModule,
-      MatListModule, 
-      MatRippleModule,
-      MatExpansionModule,
-      MatTableModule,
-      MatFormFieldModule,
-      MatPaginatorModule,
-      MatCardModule, 
-      MatSortModule,
-      MatInputModule,
-      MatTooltipModule,
-      MatDialogModule
-    ]
-  })
-  export class CommonData {
-     public readonly APIUrl="http://localhost:5000/api";
-  }
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatRippleModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatSortModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatMenuModule
+  ]
+})
+export class CommonData {
+  public readonly APIUrl = "http://localhost:5000/api";
+}
