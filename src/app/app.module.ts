117062254513c08634d9090249251e386c399949
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2022-02-03 12:25:10 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { MasterModule } from "./pages/master/master.module"; 
@@ -35,7 +36,7 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
     HttpClientModule,
     CommonData 
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
