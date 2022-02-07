d0c8c56752306a487cac2a68c8f1b25f26e9e256
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2022-02-07 11:14:09 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
+import { LoginComponent } from "./pages/account/login/login.component";
+
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent}
+
+  // {path: '', redirectTo: 'login', pathMatch: 'full'},
+  // {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
